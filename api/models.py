937fe9d48dedde111a4a64cac2f9a3d6fb0fbf48
bradsguaso <a.bradshawf07@gmail.com>
2025-03-01 14:39:45 -0500

    Super user

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    photo_url = models.URLField(max_length=500, blank=True,null=True)
+
+    def __str__(self):
+        return self.username
